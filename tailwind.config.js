0da21b7663f76a8e84828e5a7ba6891e541e158c
Mizanali Panjwani <mizanalip786@gmail.com>
2022-05-08 08:32:51 +0530

    feat: Tailwind CSS setup

---
+++ tailwind.config.js
@@ -0,0 +1,42 @@
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}'
+  ],
+  media: false, // or 'media' or 'class'
+  theme: {
+    colors: {
+      primary: '#4A96FF',
+      background: '#131319',
+      surface: '#27292D',
+      border: '#35373B',
+      gray: '#191920',
+      white: '#fff',
+      black: '#000',
+      text: '#C5C7CA',
+      muteText: '#7F8084'
+    },
+    fontFamily: {
+      sora: ['Inter', 'sans-serif']
+    },
+    fontSize: {
+      '3xs': '0.675rem',
+      '2xs': '.775rem',
+      xs: '0.8rem',
+      sm: '.875rem',
+      base: '1rem',
+      lg: '1.125rem',
+      xl: '1.25rem',
+      '2xl': '1.5rem',
+      '3xl': '1.875rem',
+      '4xl': '2.25rem',
+      '5xl': '3rem',
+      '6xl': '4rem',
+      '7xl': '5rem'
+    }
+  },
+  variants: {
+    extend: {}
+  },
+  plugins: []
+};
